1da43894b9af832a1ab52864416290ba93449055
nanikon <milkchocolatewithdaisy@gmail.com>
2023-11-12 23:48:46 +0300

    add version with non-tail recursion

--- lab1/src/Task8.hs
+++ lab1/src/Task8.hs
@@ -1,19 +1,45 @@
 module Task8 (
+    task8Recursion,
     task8_TailRec,
 ) where
 
-myMultiplyTailRec :: [Int] -> Int
-myMultiplyTailRec = myMultiply_inner 1
-  where
-    myMultiply_inner n [] = n
-    myMultiply_inner n (x : xs) = myMultiply_inner (n * x) xs
+-- Non-tail recursion
+
+task8Recursion :: [Int] -> Int
+task8Recursion [] = error "List must have 13 elements"
+task8Recursion [_] = error "List must have 13 elements"
+task8Recursion [_,_] = error "List must have 13 elements"
+task8Recursion [_,_,_] = error "List must have 13 elements"
+task8Recursion [_,_,_,_] = error "List must have 13 elements"
+task8Recursion [_,_,_,_,_] = error "List must have 13 elements"
+task8Recursion [_,_,_,_,_,_] = error "List must have 13 elements"
+task8Recursion [_,_,_,_,_,_,_] = error "List must have 13 elements"
+task8Recursion [_,_,_,_,_,_,_,_] = error "List must have 13 elements"
+task8Recursion [_,_,_,_,_,_,_,_,_] = error "List must have 13 elements"
+task8Recursion [_,_,_,_,_,_,_,_,_,_] = error "List must have 13 elements"
+task8Recursion [_,_,_,_,_,_,_,_,_,_,_] = error "List must have 13 elements"
+task8Recursion [_,_,_,_,_,_,_,_,_,_,_,_] = error "List must have 13 elements"
+task8Recursion [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13] = product [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13]
+task8Recursion (x1:x2:x3:x4:x5:x6:x7:x8:x9:x10:x11:x12:x13:xs) = max (product [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13]) (task8Recursion (x2:x3:x4:x5:x6:x7:x8:x9:x10:x11:x12:x13:xs))
+
+-- Tail recursion 
 
 task8_TailRec :: [Int] -> Int
 task8_TailRec = maxMult 0
   where
-    maxMult n [] = n
-    maxMult n (x : xs) =
-        let currentMult = myMultiplyTailRec (take 13 (x : xs))
-         in if currentMult > n
-                then maxMult currentMult xs
-                else maxMult n xs
+    maxMult _ [] = error "List must have 13 elements"
+    maxMult _ [_] = error "List must have 13 elements"
+    maxMult _ [_,_] = error "List must have 13 elements"
+    maxMult _ [_,_,_] = error "List must have 13 elements"
+    maxMult _ [_,_,_,_] = error "List must have 13 elements"
+    maxMult _ [_,_,_,_,_] = error "List must have 13 elements"
+    maxMult _ [_,_,_,_,_,_] = error "List must have 13 elements"
+    maxMult _ [_,_,_,_,_,_,_] = error "List must have 13 elements"
+    maxMult _ [_,_,_,_,_,_,_,_] = error "List must have 13 elements"
+    maxMult _ [_,_,_,_,_,_,_,_,_] = error "List must have 13 elements"
+    maxMult _ [_,_,_,_,_,_,_,_,_,_] = error "List must have 13 elements"
+    maxMult _ [_,_,_,_,_,_,_,_,_,_,_] = error "List must have 13 elements"
+    maxMult _ [_,_,_,_,_,_,_,_,_,_,_,_] = error "List must have 13 elements"
+    maxMult n [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13] = max n $ product [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13]
+    maxMult n (x1:x2:x3:x4:x5:x6:x7:x8:x9:x10:x11:x12:x13:xs) =
+      maxMult (max (product [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13]) n) (x2:x3:x4:x5:x6:x7:x8:x9:x10:x11:x12:x13:xs)
