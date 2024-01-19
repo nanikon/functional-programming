f8881b13d887f84e03e5af69c5b960cbcc884c26
nanikon <milkchocolatewithdaisy@gmail.com>
2024-01-19 18:05:19 +0300

    add propertyBased test by filled

--- lab2/test/PropertyBasedTest.hs
+++ lab2/test/PropertyBasedTest.hs
@@ -5,6 +5,7 @@ module PropertyBasedTest (
     propertyBasedTests,
 ) where
 
+import Control.Monad
 import Lib
 import Test.HUnit
 
@@ -19,4 +20,15 @@ propertyBasedTests =
         [ "binary operation must be associative" ~: ((fistHM <> secondHM) <> thirdHM) ~=? (fistHM <> (secondHM <> thirdHM))
         , "binary operation with empty must not change map" ~: fistHM ~=? fistHM <> mempty
         , "bianry operation with empty must be commutative" ~: fistHM <> mempty ~=? mempty <> fistHM
+        , "current filled must be less then inited" ~: do
+            result <-
+                foldM
+                    ( \acc e -> do
+                        let newMap = addElem acc e (1 :: Int)
+                        assertEqual ("add elem with key" ++ show e) True (getCurrentFilled newMap < 0.8)
+                        return newMap
+                    )
+                    (createHashMap 0.8 [('a', 1 :: Int)])
+                    ['b' .. 'z']
+            assertEqual "result size" 26 (getSize result)
         ]
